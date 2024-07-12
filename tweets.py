7072e2a978149e5b1f853543dca74946b41b4dd4
Ivan Zapata <zerox523@gmail.com>
2024-07-12 15:41:15 -0300

    ajuste escalas

--- tweets.py
+++ tweets.py
@@ -51,45 +51,35 @@ plt.savefig(f"{carpeta_imagenes}/{nombre_imagen}")
 print(f"Gráfica guardada en: {carpeta_imagenes}/{nombre_imagen}")
 
 
+# ########################################################################################################
 
+# # Ingresa tus credenciales de API de Twitter
+# consumer_key = "Kb3pAaHpcDG7qtWsBIqy6syYg"
+# consumer_secret = "SMGh1Xzjjxf57Nn6JkJGFyPPZri3DM4XinatGl46YFEklfOT3o"
+# access_token = "1793435649791414273-65CpVf8qWU2L8CPEk80miUNMYrd8pM"
+# access_token_secret = "6GLqfZxHw4LYh0iqYLAPfhRKg5JNHoQfDf9eBk6fAwPfs"
+# bearer_token = "AAAAAAAAAAAAAAAAAAAAAFm4twEAAAAAXhiNFki2IuVYoEPczUibGV3eyc8%3Dy2YhSC41h3Op9KhcPLpidYHdgYuMWZi2vzZlF0Vv78VI2EF8WM"
 
-# Ingresa tus credenciales de API de Twitter
-consumer_key = "Kb3pAaHpcDG7qtWsBIqy6syYg"
-consumer_secret = "SMGh1Xzjjxf57Nn6JkJGFyPPZri3DM4XinatGl46YFEklfOT3o"
-access_token = "1793435649791414273-65CpVf8qWU2L8CPEk80miUNMYrd8pM"
-access_token_secret = "6GLqfZxHw4LYh0iqYLAPfhRKg5JNHoQfDf9eBk6fAwPfs"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAFm4twEAAAAAXhiNFki2IuVYoEPczUibGV3eyc8%3Dy2YhSC41h3Op9KhcPLpidYHdgYuMWZi2vzZlF0Vv78VI2EF8WM"
+# # Autentificación en Twitter
+# # V1 Twitter API Authentication
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
+# api = tweepy.API(auth, wait_on_rate_limit=True)
 
-# Autentificación en Twitter
-# V1 Twitter API Authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
-# # V2 Twitter API Authentication
 # client = tweepy.Client(
-#     bearer_token,
-#     consumer_key,
-#     consumer_secret,
-#     access_token,
-#     access_token_secret,
-#     wait_on_rate_limit=True,
+#     consumer_key=consumer_key, 
+#     consumer_secret=consumer_secret,
+#     access_token=access_token, 
+#     access_token_secret=access_token_secret
 # )
 
-client = tweepy.Client(
-    consumer_key=consumer_key, 
-    consumer_secret=consumer_secret,
-    access_token=access_token, 
-    access_token_secret=access_token_secret
-)
+# # Upload image to Twitter. Replace 'filename' your image filename.
+# media_id = api.media_upload(filename="/home/caudalimetro/imagen/grafica_rio_yi.jpeg").media_id_string
+# print(media_id)
 
-# Upload image to Twitter. Replace 'filename' your image filename.
-media_id = api.media_upload(filename="/home/caudalimetro/imagen/grafica_rio_yi.jpeg").media_id_string
-print(media_id)
+# # Text to be Tweeted
+# text = "Datos Monitoreo Río YI"
 
-# Text to be Tweeted
-text = "Datos Monitoreo Río YI"
-
-# Send Tweet with Text and media ID
-client.create_tweet(text=text, media_ids=[media_id])
-print("Tweeted!")
+# # Send Tweet with Text and media ID
+# client.create_tweet(text=text, media_ids=[media_id])
+# print("Tweeted!")
